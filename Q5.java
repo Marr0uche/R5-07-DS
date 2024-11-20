3feb607cbb45cf195c0b4394ffda5e83e7cc32cc
Marrouche Mohamad <113200905+Marr0uche@users.noreply.github.com>
2024-11-20 13:35:39 +0000

    Création de Q5.java

---
+++ Q5.java
@@ -0,0 +1,12 @@
+public class Q5 {
+
+    public static void main(String[] args) {
+
+        for (int i = Integer.parseInt(args[0]); i > 0 ; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("x");
+            }
+            System.out.println();
+        }
+    }
+}
